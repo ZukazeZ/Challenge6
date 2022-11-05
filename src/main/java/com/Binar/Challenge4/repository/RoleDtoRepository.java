40f231a8285ee4269eeab76d42eff1e770ecb741
Nico <nicoramli@gmail.com>
2022-11-05 21:16:59 +0700

    Challenge 7.2

--- src/main/java/com/Binar/Challenge4/repository/RoleDtoRepository.java
+++
@@ -1,9 +0,0 @@
-//package com.binar.challenge4.repository;
-//
-//import com.binar.challenge4.dto.RoleDto;
-//import org.springframework.data.jpa.repository.JpaRepository;
-//import org.springframework.stereotype.Repository;
-//
-//@Repository
-//public interface RoleDtoRepository extends JpaRepository<RoleDto,Long> {
-//}
